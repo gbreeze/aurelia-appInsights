7f3afbe270ac57dae320c382ab8a19f9affb9bbf
George Breeze <mal@georgebreeze.com>
2016-01-16 17:19:31 +0100

    cleanup

--- Aurelia-AppInsights/test/unit/modules.d.ts
+++ Aurelia-AppInsights/test/unit/modules.d.ts
@@ -1,9 +1,4 @@
-﻿declare module "resources/aurelia-appInsights" {
-	import mod = require("src/resources/aurelia-appInsights");
-	export = mod;
-}
-
-declare module "views/welcome" {
+﻿declare module "views/welcome" {
     import w = require("src/views/welcome");
     export = w;
 }
